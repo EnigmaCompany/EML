6a3f6583966aa4eaaccb3da7ee922af3de1355dd
Paul Lee <hillcrestpaul0719@gmail.com>
2018-03-13 15:19:16 -0700

    update iris.py to use the web dataset, and to better classify items using sureness

--- grouse/examples/iris.py
+++ grouse/examples/iris.py
@@ -1,8 +1,29 @@
-sepal_len = [(6.4, 3), (5.0, 2), (4.9, 3), (4.9, 1), (5.7, 1)]
-sepal_wid = [(2.8, 3), (2.3, 2), (2.5, 3), (3.1, 1), (3.8, 1)]
-petal_len = [(5.6, 3), (3.3, 2), (4.5, 3), (1.5, 1), (1.7, 1)]
-petal_wid = [(2.2, 3), (1.0, 2), (1.7, 3), (0.1, 1), (0.3, 1)]
-choices = {1: "setosa", 2: "versicolor", 3: "virginica"}
+import requests, pprint
+
+# sepal_len = [(6.4, 3), (5.0, 2), (4.9, 3), (4.9, 1), (5.7, 1)]
+# sepal_wid = [(2.8, 3), (2.3, 2), (2.5, 3), (3.1, 1), (3.8, 1)]
+# petal_len = [(5.6, 3), (3.3, 2), (4.5, 3), (1.5, 1), (1.7, 1)]
+# petal_wid = [(2.2, 3), (1.0, 2), (1.7, 3), (0.1, 1), (0.3, 1)]
+
+choices = {1: "Iris-setosa", 2: "Iris-versicolor", 3: "Iris-virginica"}
+
+inv_choices = {v: k for k, v in choices.items()}
+lists = [[], [], [], []]
+data = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data').text
+data = [i.split(",") for i in data.strip("\n").split("\n")]
+# print(data)
+for i in range(0, len(data[0]) - 1):
+	for j in data:
+		lists[i].append((float(j[i]), int(inv_choices[j[4]])))
+
+# for i in lists:
+# 	pprint.pprint(i)
+# 	print("====================")
+
+sepal_len = lists[0]
+sepal_wid = lists[1]
+petal_len = lists[2]
+petal_wid = lists[3]
 
 import sys
 import statistics
@@ -28,8 +49,20 @@ for i in range(0, len(predict_x[0])):
 	responses.append(gf.grouse(petal_len, [1, 2, 3], predict_x[2][i]))
 	responses.append(gf.grouse(petal_wid, [1, 2, 3], predict_x[3][i]))
 	responses_abstract = [i[0] for i in responses]
-	chosen = statistics.mode(responses_abstract)
-	sure = [i[1] for i in responses if chosen == i[0]]
+	# chosen = statistics.mode(responses_abstract)
+	computed = []
+	new_responses = []
+	for i in responses:
+		computed.append(i[0])
+		filtered = [j for j in responses if j[0] == i[0]]
+		filtered_pos = [i[1] for i in filtered]
+		for i in filtered:
+			responses.remove(i)
+		new_responses.append((i[0], statistics.mean(filtered_pos)))
+	new_responses.sort(key=lambda x: x[1])
+	chosen = new_responses[-1]
+	# sure = [i[1] for i in responses if chosen == i[0]]
 	# print(responses)
 	# print(responses_abstract)
-	print(choices[chosen] + " - " + str(statistics.mean(sure)) + "% sure.")
+	# print(choices[chosen] + " - " + str(statistics.mean(sure)) + "% sure.")
+	print(choices[chosen[0]] + " - " + str(chosen[1]) + "% sure.")
