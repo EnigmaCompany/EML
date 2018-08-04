051bc8c82cd1723b14e2a6a4203083dae56f0bfd
Paul Lee <enigma@enigmatic.network>
2018-08-04 13:26:14 -0400

    traning works but only with first example

--- v3_campluther/campluther.py
+++ v3_campluther/campluther.py
@@ -1,23 +1,35 @@
 import random
+import time
 
 class CampLuther:
-    def __init__(self, input_data_num):
+    def __init__(self, input_data_num, save, target_loss=1):
         self.input_data_num = input_data_num
-        self.mode = None
-        self.variables = []
 
-        if not input_data_num == 1:
-            var_num = self.input_data_num * 4 + 3
-        else:
-            var_num = 3
+        try:
+            save_file = open(save, "r")
+            self.variables = eval(save_file.read())
+        except Exception as e:
+            print(str(e))
+            self.variables = []
 
-        for i in range(0, var_num):
-            self.variables.append(random.randint(0, 10))
+            if not input_data_num == 1:
+                var_num = self.input_data_num * 4 + 3
+            else:
+                var_num = 3
+
+            for i in range(0, var_num):
+                self.variables.append(random.randint(0, 50))
+
+            save_file = open(save, "w")
+            save_file.write(str(self.variables))
 
         self.loss = None
         self.var_to_be_changed = 0
         self.equation_structure  = self._calcEquationStructure()
         self.shift_value = 1
+        self.done_train_steps = 0
+        self.target_loss = target_loss
+        self.save = save
 
     def _calcEquationStructure(self):
         # for cur_len in range(1, self.input_data_num + 1):
@@ -39,16 +51,91 @@ class CampLuther:
         newVar.insert(self.var_to_be_changed, newVar.pop(self.var_to_be_changed) - self.shift_value)
         return newVar
 
-    def train_one(self, training_data=None):
-        pass
+    def train_core(self, training_data=None, quiet=False, delay=0):
+        for i in training_data:
+            try:
+                target_reached_status = self.loss < self.target_loss
+            except:
+                target_reached_status = False
+            # last_vars = None
+            last_loss = None
+            while not target_reached_status:
+                error = None
+                try:
+                    higher_equation_result = self.calculate(i[0], self.higherEquation())
+                    higher_equation_loss = abs(higher_equation_result - i[1])
+                except:
+                    error = "h"
+                try:
+                    lower_equation_result = self.calculate(i[0], self.lowerEquation())
+                    lower_equation_loss = abs(lower_equation_result - i[1])
+                except:
+                    error = "l"
+                if not error == None:
+                    if error == "l":
+                        self.variables = self.higherEquation()
+                        self.loss = higher_equation_loss
+                    else:
+                        self.variables = self.lowerEquation()
+                        self.loss = lower_equation_loss
+                else:
+                    if higher_equation_loss < lower_equation_loss:
+                        self.variables = self.higherEquation()
+                        self.loss = higher_equation_loss
+                    else:
+                        self.variables = self.lowerEquation()
+                        self.loss = lower_equation_loss
 
-    def train(self, train_steps=None, traning_data=None):
-        if train_steps == None:
-            iterObj = True
-        else:
-            iterObj = range(0, train_steps)
-        while iterObj:
-            train_one(traning_data)
+                if self.var_to_be_changed == len(self.variables) - 1:
+                    self.var_to_be_changed = 0
+                # elif self.var_to_be_changed = 0:
+                #     if not last_vars == None:
+                #         if last_vars == self.variables:
+                #             self.shift_value /= 10
+                #     print(last_vars)
+                #     last_vars = self.variables
+                else:
+                    self.var_to_be_changed += 1
+
+                # print(last_loss)
+
+                # if not last_loss == None:
+                #     # print(abs(last_loss - self.loss))
+                #     # print(self.shift_value)
+                #     # if abs(last_loss - self.loss) < (self.shift_value):
+                #     if self.loss < (self.shift_value):
+                #         self.shift_value /= 10
+                #         # print(self.shift_value)
+                #     # elif last_loss == self.loss:
+                #     #     self.shift_value /= 2
+                #     elif self.loss > (self.shift_value * 10):
+                #         self.shift_value *= 10
+
+                if self.loss < self.shift_value:
+                    self.shift_value /= 10
+
+                last_loss = self.loss
+
+                self.done_train_steps += 1
+                if not quiet:
+                    print("Step " + str(self.done_train_steps) + " done. Loss: " + str(self.loss) + " Last Loss: " + str(last_loss) + " Shift Value: " + str(self.shift_value) + " Variables: " + str(self.variables))
+                    pass
+                if str(self.done_train_steps)[-5:] == "00000":
+                    save_file = open(self.save, "w")
+                    save_file.write(str(self.variables))
+
+                time.sleep(delay)
+
+
+    def train(self, train_steps=None, traning_data=None, quiet=False, delay=0):
+        # if train_steps == None:
+        #     iterObj = True
+        # else:
+        #     iterObj = range(0, train_steps)
+        # while iterObj:
+        #     self.train_one(traning_data, quiet, delay)
+        print(quiet)
+        self.train_core(traning_data, quiet, delay)
 
     def calculate(self, input_data, variables=None):
         if variables == None:
